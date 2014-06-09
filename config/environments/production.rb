2e0ca595b908aea4555f8873ff9d9e88319623e6
Jonnyread <jonnyread@yahoo.com>
2014-06-09 08:55:18 -0500

    added devise and user

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Need to set this up for actual host
+  config.action_mailer.default_url_options = { :host => 'nutrionista.herokuapp.com' }
 end
