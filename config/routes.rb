37425f2ca09c0e2432e3d78a269c89f141c52c15
Jonnyread <jonnyread@yahoo.com>
2014-06-08 19:37:32 -0500

    Staging for Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root 'pages#home'
 
-  get 'about' => 'pages#about'
+  get 'about' => 'pages#about' #creates about path
+  get 'goals' => 'pages#goals' #creates goals path
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
