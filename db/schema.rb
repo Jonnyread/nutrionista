eda1e419aad6186fcccc272cd9cb53fa06e44872
Jonnyread <jonnyread@yahoo.com>
2014-06-09 11:45:29 -0500

    user and meal associations added

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140609150438) do
+ActiveRecord::Schema.define(version: 20140609163413) do
 
   create_table "meals", force: true do |t|
     t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
+
+  add_index "meals", ["user_id"], name: "index_meals_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
